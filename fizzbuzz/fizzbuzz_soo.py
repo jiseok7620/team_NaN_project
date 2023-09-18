cd6b58bf7bd4fbc7019d999477c8518b1b813f04
sooyoung-wind <sooyoung.wind@gmail.com>
2023-09-18 17:38:08 +0900

    fix: Combine fizz, buzz, and fizzbuzz on branch dev
    개별로 동작하는 fizz, buzz, fizzbuzz를 전부다 하나로 통합

--- fizzbuzz/fizzbuzz_soo.py
+++ fizzbuzz/fizzbuzz_soo.py
@@ -4,9 +4,9 @@
 for number in range(0,30):
     if number % 15 == 0:
         print('fizzbuzz')
-    if number % 3 == 0:
+    elif number % 3 == 0:
         print('fizz')
-    if number % 5 == 0:
+    elif number % 5 == 0:
         print('buzz')
     else:
         print(number)
